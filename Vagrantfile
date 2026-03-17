978f3166a4f85c3a3ea8bae19ef480e7d64e9209
matan77 <matan54828482@gmail.com>
2026-03-17 18:59:08 +0200

    feat: add memory to vm

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,10 @@ Vagrant.configure("2") do |config|
   config.vm.box = "almalinux/9"
   config.vm.provision "shell", path: "./config/init.sh"
 
+  config.vm.provider "virtualbox" do |vb|
+    vb.memory = "4096"
+  end
+
   config.vm.define "server" do |server|
     server.vm.hostname = "server"
     server.vm.network "private_network", ip: "192.168.33.101"
--- config/init.sh
+++ config/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo dnf config-manager --set-enabled crb
+sudo dnf install -y epel-release
 sudo dnf update -y
-sudo dnf install httpd -y
 
 # db setup PostgreSQL
 sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
@@ -9,7 +10,10 @@ sudo dnf install -y postgresql17-server
 sudo /usr/pgsql-17/bin/postgresql-17-setup initdb
 sudo systemctl enable postgresql-17
 sudo systemctl start postgresql-17
-
-
+ 
 # create user called bugs for bugzilla, run as user postgres (super user of postgresql)
 sudo -u postgres psql -c "CREATE USER bugs WITH CREATEDB NOCREATEROLE NOSUPERUSER PASSWORD 'bugs_pass';"
+sudo -u postgres psql -c "CREATE DATABASE bugs OWNER bugs;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE bugs TO bugs;"
+
+sudo dnf install -y perl mod_perl httpd bugzilla
